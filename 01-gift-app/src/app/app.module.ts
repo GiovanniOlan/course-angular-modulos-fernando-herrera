c886190b8b5d32e7ac9d11fef315cfeb98e52282
GiovanniOlan <giovanniolanperez@gmail.com>
2025-01-28 11:47:16 +0000

    Creating sidebar component

--- 01-gift-app/src/app/app.module.ts
+++ 01-gift-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 01-gift-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  standalone: false
+})
+export class SidebarComponent { }
