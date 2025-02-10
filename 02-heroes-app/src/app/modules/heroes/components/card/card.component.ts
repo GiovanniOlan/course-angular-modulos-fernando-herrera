d9eedae61661344256e299a283da544e990d1b03
Giovanni Olan Perez <giovanniolanperez.fp@gmail.com>
2025-02-10 16:13:17 +0000

    Add hero card component and update hero list layout

---
+++ 02-heroes-app/src/app/modules/heroes/components/card/card.component.ts
@@ -0,0 +1,62 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'heroes-heroe-card',
+  standalone: false,
+  template: `
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>{{ hero.superhero }}</mat-card-title>
+        <mat-card-subtitle>{{ hero.alter_ego }}</mat-card-subtitle>
+      </mat-card-header>
+      <!-- <img mat-card-image [src]="hero.images.md" alt="{{ hero.name }}"> -->
+      <mat-card-content class="mt-2">
+        <h4>{{ hero.publisher }}</h4>
+        <p>
+          <strong>Primero aparición</strong> {{ hero.first_appearance }}
+          <br>
+          <mat-chip-listbox>
+            <mat-chip *ngFor=" let name of hero.characters.split(',') | slice:0:3 ">{{ name }}</mat-chip>
+          </mat-chip-listbox>
+        </p>
+      </mat-card-content>
+
+      <mat-card-actions>
+        <button
+          mat-button
+          mat-raised
+          color="primary"
+          [routerLink]="['/heroes/edit', hero.id]"
+        >
+          <mat-icon>edit</mat-icon>
+          Editar
+        </button>
+
+        <span class="spacer"></span>
+
+        <button
+          mat-button
+          mat-raised
+          [routerLink]="['/heroes', hero.id]"
+        >
+          <mat-icon>more_horizontal</mat-icon>
+          Más...
+        </button>
+      </mat-card-actions>
+    </mat-card>
+  `,
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+
+  @Input({
+    required: true
+  })
+  public hero!: Hero;
+}
--- 02-heroes-app/src/app/modules/heroes/heroes.module.ts
+++ 02-heroes-app/src/app/modules/heroes/heroes.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { PageLayoutModule } from '../../layouts/page-layout/page-layout.module';
+import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
   declarations: [
     ListPageComponent,
+    CardComponent
   ],
   imports: [
     // Third modules
     CommonModule,
+    MaterialModule,
     // Layouts
     PageLayoutModule,
     // My internal modules
